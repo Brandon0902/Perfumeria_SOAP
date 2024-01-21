7544d0e3b2a366c80f49ed3988869b7e5b0e583d
Eric <eric.leo04@gmail.com>
2024-01-20 22:37:20 -0600

    cree las tablas Brand, Customers y Shippers

--- database/migrations/2024_01_21_040947_create_brands_table.php
+++ database/migrations/2024_01_21_040947_create_brands_table.php
@@ -11,10 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('brands', function (Blueprint $table) {
+            $table->id();
+            $table->String("name",15);
+            $table->String("Description",125);
+            $table->String("Category",20);
+
+
+            $table->foreign('category_id')->references('id')->on('categories');
+
         });
     }
 
@@ -23,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('brands');
     }
 };
