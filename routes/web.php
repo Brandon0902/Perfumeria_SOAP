5837eb12269296c39a8118dde7691db4ccdc4c43
Brandon Lopez <brandonaxellopezdealba@gmail.com>
2024-01-20 23:36:46 -0600

    Borre la migración orders para acomodarla

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,10 +22,18 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::view('/products', '/administrador/products/create')->name('products');
+    Route::view('/brands', '/administrador/brands/create')->name('brands');
+    Route::view('/Shippers', '/administrador/shippers/create')->name('shippers');
+    Route::view('/categorias', '/administrador/categorias/create')->name('categorias');
 });
 
+//Route::view('/inicio','/administrador/inicio/inicio');
+
 require __DIR__.'/auth.php';
+
